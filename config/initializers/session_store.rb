f7e114c8c5b1e79de062f5eb31380fbed3e94518
Jorge Téllez <novohispano@gmail.com>
2015-12-07 10:54:08 -0700

    Initial setup

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_matt_and_beth_wedding_party_for_all_the_cool_people_at_turing_session'
